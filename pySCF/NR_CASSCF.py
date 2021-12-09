7a501c09270c2273edab36a17308456a07fcd656
antoine-marie <antoine.marie@ens-lyon.fr>
2021-12-09 18:01:46 +0100

    Adding methods to create the CASSCF gradient, going to test/debug tomorrow

--- pySCF/NR_CASSCF.py
+++ pySCF/NR_CASSCF.py
@@ -42,7 +42,7 @@ def get_CASRDM_12(self,ci,ncas,nelecas):
 
     return dm1_cas.T, dm2_cas # We transpose the 1-RDM because their convention is <|a_q^\dagger a_p|>
 
-def CASRDM1_to_RDM1(casdm1,norb,ncore,ncas): #TODO
+def CASRDM1_to_RDM1(casdm1,norb,ncore,ncas):
     dm1 = np.zeros((norb,norb))
     dm1_core = 2*np.diag(np.ones(ncore)) #the occocc part of dm1 is a diagonal of 2
     dm1[:ncore,:ncore] = dm1_core
@@ -60,7 +60,7 @@ def dm2_mo_occ(part,p,q,r,s,dm1):
     else:
         return('Wrong specification of part')
 
-def CASRDM2_to_RDM2(dm1_cas,dm2_cas,norb,ncore,ncas): #TODO
+def CASRDM2_to_RDM2(dm1_cas,dm2_cas,norb,ncore,ncas):
     dm2 = np.zeros((norb,norb,norb,norb))
     for i in range(ncore):      # really not elegant ... find an alternative way to do this
         for j in range(ncore):
@@ -106,8 +106,7 @@ def get_tCASRDM12(self,ci1,ci2,ncas,nelecas):
         nDeta = scipy.special.comb(ncas,nelecas[0])
         nDetb = scipy.special.comb(ncas,nelecas[1])
         ci2 = ci2.reshape((nDeta,nDetb)) #TODO be careful that the reshape is the inverse of the "unfolding" of the matrix vector
-
-     t_dm1_cas, t_dm2_cas = mycas.fcisolver.trans_rdm12(ci1,ci2,ncas,nelecas)
+    t_dm1_cas, t_dm2_cas = mycas.fcisolver.trans_rdm12(ci1,ci2,ncas,nelecas)
 
     return t_dm1_cas, t_dm2_cas #TODO do we need to transpose this matrix
 
@@ -129,6 +128,35 @@ def get_F_cas(norb,ncas,dm1_cas,eri): #TODO same as above
                     F_cas[p,q] += dm1_cas[t,u]*(eri[p,q,t,u]-0.5*eri[p,u,t,q])
     return(F_cas)
 
+def get_GradOrb(self,F_core,F_cas,dm1_cas,dm2_cas):
+    g_orb = np.zeros((self.norb,self.norb))
+    ncore = self.ncore
+    ncas = self.ncas
+    nocc = ncore + ncas
+
+    g_orb[nocc:,:ncore] = 4*(F_core + F_cas)[nocc:,:ncore]
+    g_orb[nocc:,ncore:nocc] = 4*(F_core + F_cas)[nocc:,ncore:nocc] - 2*np.einsum('tv,iv->ti',dm1_cas,F_core[nocc:,ncore:nocc]) - 4*np.einsum('tvxy,ivxy->ti',dm2_cas,eri)
+    g_orb[ncore:nocc,:ncore] = 2*np.einsum('tv,av->ta',dm1_cas,F_core[ncore:nocc,:ncore]) + 4*np.einsum('tvxy,avxy->ta',dm2_cas,eri)
+
+    return g_orb - g_orb.T # this gradient is a matrix, be careful we need to pack it before joining it with the CI part
+
+def get_gradCI(self,ci_mat,h1e,eri):
+    g_ci = np.zeros(len(ci_mat)-1)
+    ciO = ci_mat[:,0]
+
+    for i in range(len(ci_mat)-1):
+        t_dm1_cas, t_dm2_cas = self.get_tCASRDM12(self,ci_mat[:,i+1],ciO)
+        t_dm1 = CASRDM1_to_RDM1(t_dm1_cas,self.norb,self.ncore,self.ncas)
+        t_dm2 = CASRDM2_to_RDM2(t_dm1_cas,t_dm2_cas,self.norb,self.ncore,self.ncas)
+
+        g_ci[i] = 2*np.einsum('pq,pq',h1e,t_dm1) + np.einsum('pqrs,pqrs',eri,t_dm2)
+
+    return g_ci
+
+def form_Grad(self,g_orb,g_ci):
+    uniq_g_orb = self.pack_uniq_var(g_orb)
+    g = np.concatenate((g_orb,g_ci))
+    return g
 
 ##### Definition of the class #####
 
@@ -169,21 +197,23 @@ class NR_CASSCF(lib.StreamObject):
         self.stdout = mol.stdout
         self.max_memory = myhf.max_memory
         self.ncas = ncas
-        if isinstance(nelecas, (int, numpy.integer)):
+        if isinstance(nelecas, (int, np.integer)):
             nelecb = (nelecas-mol.spin)//2
             neleca = nelecas - nelecb
             self.nelecas = (neleca, nelecb)
         else:
             self.nelecas = (nelecas[0],nelecas[1])
+        self.nDeta = scipy.special.comb(self.ncas,self.nelecas[0])
+        self.nDetb = scipy.special.comb(self.ncas,self.nelecas[1])
+        self.nDet = self.nDeta*self.nDetb
 
-        self.ncore = ncore
-        self.initMO = initMO
-        self.initCI = initCI
+        self.v1e = mol.intor('int1e_nuc') #Nuclear repulsion matrix elements
+        self.t1e = mol.intor('int1e_kin') #Kinetic energy matrix elements
+        self.h1e_AO =  self.t1e + self.v1e
+        self.eri_AO = mol.intor('int2e') #eri in the AO basis in the chemist notation
         self.frozen = frozen
 
-        singlet = (getattr(__config__, 'mcscf_casci_CASCI_fcisolver_direct_spin0', False)
-                   and self.nelecas[0] == self.nelecas[1])  # leads to direct_spin1
-        self.fcisolver = fci.solver(mol, singlet, symm=False)
+        self.fcisolver = fci.direct_spin0_symm.FCISolver(mol)
 
     @property
     def ncore(self):
@@ -198,22 +228,26 @@ class NR_CASSCF(lib.StreamObject):
     @property
     def initMO(self):
         if self._initMO is None:
+            self.mo_coeff = myhf.mo_coeff
+            self.h1e = np.einsum('ip,ij,jq->pq', self.mo_coeff, self.h1e_ao, self.mo_coeff) # We transform the 1-electron integrals to the MO basis
+            self.eri = np.asarray(mol.ao2mo(self.mo_coeff)) # eri in the MO basis as super index matrix (ij|kl) with i>j and k>l VERIFY THIS LAST POINT
+            self.eri = ao2mo.restore(1, self.eri, norb) # eri in the MO basis with chemist notation
             return myhf.mo_coeff
         else:
+            self.mo_coeff = initMO
+            self.h1e = np.einsum('ip,ij,jq->pq', self.mo_coeff, self.h1e_ao, self.mo_coeff) # We transform the 1-electron integrals to the MO basis
+            self.eri = np.asarray(mol.ao2mo(self.mo_coeff)) # eri in the MO basis as super index matrix (ij|kl) with i>j and k>l VERIFY THIS LAST POINT
+            self.eri = ao2mo.restore(1, self.eri, norb) # eri in the MO basis with chemist notation
             return self._initMO
 
-    @property #TODO what do choose as the default initialisation CI vectors matrix
+    @property #TODO add an option to initialize as a full CASCI diagonalization instead of a diagonal of 1
     def initCI(self):
         if self._initCI is None:
-            pass
+            self.CI_mat = np.diag(np.ones(nDet))
+            return np.diag(np.ones(nDet))
         else:
-            return
-
-    if initMO is None:
-        self.mo_coeff = myhf.mo_coeff
-    else:
-        self.mo_coeff = initMO
-
+            self.CI_mat = initCI
+            return self._initCI
 
     def dump_flags(self): #TODO
         '''
@@ -226,58 +260,85 @@ class NR_CASSCF(lib.StreamObject):
         pass
 
     def energy_nuc(self):
-        """ Retrieve the nuclear energy from the SCF instance """
+        ''' Retrieve the nuclear energy from the SCF instance '''
         return self._scf.energy_nuc()
 
     def get_hcore(self, mol=None):
-        """ Retrieve the 1 electron Hamiltonian from the SCF instance """
+        ''' Retrieve the 1 electron Hamiltonian from the SCF instance '''
         return self._scf.get_hcore(mol)
 
-    def get_CASRDM_1(self,ci,ncas,nelecas):
+    def get_CASRDM_1(self,ci):
         ''' This method compute the 1-RDM in the CAS space of a given ci wave function '''
-        return get_CASRDM_1(self,ci,ncas,nelecas)
+        return get_CASRDM_1(self,ci,self.ncas,self.nelecas)
 
-    def get_CASRDM_12(self):
+    def get_CASRDM_12(self,ci):
         ''' This method compute the 1-RDM and 2-RDM in the CAS space of a given ci wave function '''
-        return get_CASRDM_12(self,ci,ncas,nelecas)
+        return get_CASRDM_12(self,ci,self.ncas,self.nelecas)
 
-    @staticmethod
-    def CASRDM1_to_RDM1(casdm1,norb,ncore,ncas): #TODO
+    def CASRDM1_to_RDM1(self,casdm1):
         ''' This method takes a 1-RDM in the CAS space and transform it to the full MO space '''
-        return CASRDM1_to_RDM1(casdm1,norb,ncore,ncas)
+        return CASRDM1_to_RDM1(casdm1,self.norb,self.ncore,self.ncas)
 
-    @staticmethod
-    def CASRDM2_to_RDM2(casdm2,norb,ncore,ncas): #TODO
+    def CASRDM2_to_RDM2(self,casdm2):
         ''' This method takes a 2-RDM in the CAS space and transform it to the full MO space '''
-        return CASRDM2_to_RDM2(casdm2,norb,ncore,ncas)
+        return CASRDM2_to_RDM2(casdm2,self.norb,self.ncore,self.ncas)
 
-
-    def get_tCASRDM1(self,ci1,ci2,ncas,nelecas):
+    def get_tCASRDM1(self,ci1,ci2):
         ''' This method compute the 1-electron transition density matrix between the ci vectors ci1 and ci2 '''
-        return get_tCASRDM1(self,ci1,ci2,ncas,nelecas)
+        return get_tCASRDM1(self,ci1,ci2,self.ncas,self.nelecas)
 
-    def get_tCASRDM12(self):
+    def get_tCASRDM12(self,ci1,ci2):
         ''' This method compute the 1- and 2-electrons transition density matrix between the ci vectors ci1 and ci2 '''
-        return get_tCASRDM12(self,ci1,ci2,ncas,nelecas)
+        return get_tCASRDM12(self,ci1,ci2,self.ncas,self.nelecas)
 
-    @staticmethod
-    def get_genFock(self,norb,ncore,ncas,h1e,eri,dm1_cas): #TODO
+    def get_genFock(self,dm1_cas):
         ''' This method build the generalized Fock matrix '''
-        return get_F_core(norb,ncore,h1e,eri) + get_F_cas(norb,ncas,dm1_cas,eri)
+        return get_F_core(self.norb,self.ncore,self.h1e,self.eri) + get_F_cas(self.norb,self.ncas,dm1_cas,self.eri)
 
-    def get_GradOrb(self) #TODO add an if statement for MCSCF case
+    def uniq_var_indices(self, nmo, frozen):
+        ''' This function creates a matrix of boolean of size (norb,norb). A True element means that this rotation should be taken into account during the optimization. Taken from pySCF.mcscf.casscf '''
+        nocc = self.ncore + self.ncas
+        mask = np.zeros((self.norb,self.norb),dtype=bool)
+        mask[ncore:nocc,:ncore] = True # Active-Core rotations
+        mask[nocc:,:nocc] = True # Virtual-Core and Virtual-Active rotations
+        # if self.internal_rotation:
+        #    mask[ncore:nocc,ncore:nocc][np.tril_indices(ncas,-1)] = True
+        if frozen is not None:
+            if isinstance(frozen, (int, np.integer)):
+                mask[:frozen] = mask[:,:frozen] = False
+            else:
+                frozen = np.asarray(frozen)
+                mask[frozen] = mask[:,frozen] = False
+        return mask
+
+    def pack_uniq_var(self, mat):
+        ''' This method transforms a matrix of rotations K into a list of unique rotations elements. Taken from pySCF.mcscf.casscf '''
+        nmo = self.mo_coeff.shape[1]
+        idx = self.uniq_var_indices(nmo, self.ncore, self.ncas, self.frozen)
+        return mat[idx]
+
+    # to anti symmetric matrix
+    def unpack_uniq_var(self, v):
+        ''' This method transforms a list of unique rotations elements into an anti-symmetric rotation matrix. Taken from pySCF.mcscf.casscf '''
+        nmo = self.mo_coeff.shape[1]
+        idx = self.uniq_var_indices(nmo, self.ncore, self.ncas, self.frozen)
+        mat = np.zeros((nmo,nmo))
+        mat[idx] = v
+        return mat - mat.T
+
+    def get_GradOrb(self,F_core,F_cas,dm1_cas,dm2_cas): #TODO add an if statement for MCSCF case
         ''' This method build the orbital part of the gradient '''
-        pass
+        return get_GradOrb(self,F_core,F_cas,dm1_cas,dm2_cas)
 
-    def get_GradCI(self) #TODO
+    def get_GradCI(self,ci_mat):
         ''' This method build the CI part of the gradient '''
-        pass
+        return get_gradCI(self,ci_mat,self.h1e,self.eri)
 
-    def get_Grad(self) #TODO
+    def form_Grad(self,g_orb,g_ci):
         ''' This method concatenate the orbital and CI part of the gradient '''
-        pass
+        return form_Grad(self,g_orb,g_ci)
 
-    def get_Hessian(self) #TODO
+    def get_Hessian(self): #TODO
         ''' This method concatenate the orb-orb, orb-CI and CI-CI part of the Hessian '''
         pass
 
@@ -287,3 +348,13 @@ class NR_CASSCF(lib.StreamObject):
 
 ##### Main #####
 if __name__ == '__main__':
+
+    mol = pyscf.M(
+        atom = 'H 0 0 0; H 0 0 1.2',
+        basis = '6-31g')
+
+    myhf = mol.RHF().run()
+
+    mycas = NR_CASSCF(myhf,2,2)
+
+
