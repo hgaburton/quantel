318f3005f4b9661f26c94ea0e980c60213845afe
Hugh Burton <hb407@cam.ac.uk>
2022-01-18 20:18:47 +0000

    functioning overlap

--- pySCF/gnme/utils.py
+++ pySCF/gnme/utils.py
@@ -1,5 +1,35 @@
 #!/usr/bin/python3 
 
+import unittest
+import numpy as np
+
+def orthogonalisation_matrix(M, thresh=1e-8):
+
+    # Get eigenvalues and eigenvectors
+    eigval, eigvec = np.linalg.eigh(M)
+
+    # Identify non-null space
+    inds = np.argwhere(np.abs(eigval) > thresh).flatten()
+
+    # Construct orthogonalisation matrix
+    X = eigvec[:,inds].dot(np.diag(np.power(eigval[inds],-0.5)))
+
+    return X
+
+def orthogonalise(C,S,thresh=1e-8):
+    '''Orthogonalise a set of occipied orbitals C with respect to the overlap matrix S'''
+
+    # Get MO overlap matrix
+    Smo = np.einsum('pi,pq,qj->ij',np.conj(C),S,C)
+
+    # Get orthogonalisation matrix
+    X = orthogonalisation_matrix(Smo, thresh=thresh)
+
+    # Orthogonalise the orbitals
+    C_new = np.einsum('pi,ij->pj',C,X)
+
+    return C_new
+
 def lowdin_pair(Cw,Cx,S,thresh=1e-10):
     '''Perform Lowdin pairing on two sets of *occupied* orbitals defined in Cw (bra) and Cx (ket).
 
@@ -32,15 +62,15 @@ def lowdin_pair(Cw,Cx,S,thresh=1e-10):
     if(np.linalg.norm(diag_test) > thresh):
         # Construct transformation matrices using SVD
         U, D, VT = np.linalg.svd(Swx)
-        V = np.asmatrix(VT).H
+        V = np.conj(VT).T
 
         # Transform the orbitals
         Cw_new = np.einsum('pi,ij->pj',Cw,U)
-        Cx_new = np.einsum('pi,ij->pj',Cw,V)
+        Cx_new = np.einsum('pi,ij->pj',Cx,V)
 
         # Correct for any new phase
-        Cw_new[:,0] *= np.linalg.det(np.asmatrix(U).H)
-        Cx_new[:,0] *= np.linalg.det(np.asmatrix(V).H)
+        Cw_new[:,0] *= np.linalg.det(np.conj(U).T)
+        Cx_new[:,0] *= np.linalg.det(np.conj(V).T)
 
         # Compute new overlap matrix
         Swx = np.einsum('pi,pq,qj->ij',np.conj(Cw_new),S,Cx_new)
@@ -81,3 +111,50 @@ def reduced_overlap(Sxx, thresh=1e-8):
     nzero = len(zeros)
 
     return invSxx, redOV, zeros, nzero
+
+
+class test_utils(unittest.TestCase):
+    def get_random_coeff(self,nb,ne):
+        return np.random.rand(nb,ne)
+
+    def get_random_overlap(self,nb):
+        # Fill with random numbers
+        S  = np.random.rand(nb,nb)
+        # Make sure it's symmetric
+        S  = 0.5 * (S + S.T)
+        S  = S - np.diag(np.diag(S)) + np.eye(nb)
+        # Make sure it's positive definite
+        sig, U = np.linalg.eigh(S)
+        S  = np.einsum('pq,qr,rs->ps',U,np.diag(np.abs(sig)),U.T)
+        return S
+
+    def test_lowdin_pair(self):
+
+        # Set some number of electrons (ne) and basis functions (nb)
+        ne,nb = 10, 5
+
+        # Get random coefficients
+        Cw = self.get_random_coeff(nb,ne)
+        Cx = self.get_random_coeff(nb,ne)
+        # Get random overlap matrix
+        S  = self.get_random_overlap(nb)
+        # Orthogonalise reference orbitals
+        Cw = orthogonalise(Cw,S)
+        Cx = orthogonalise(Cx,S)
+
+        # Get initial overlap matrix
+        Sold = np.einsum('pi,pq,qj',np.conj(Cw),S,Cx)
+
+        # Perform the Lowdin-pairing
+        Cw_new, Cx_new, Sxx = lowdin_pair(Cw,Cx,S)
+
+        # Test if we actually have diagonal orbitals
+        Snew = np.einsum('pi,pq,qj',np.conj(Cw_new),S,Cx_new)
+        diag_test = Snew - np.diag(np.diag(Snew))
+        self.assertTrue(np.linalg.norm(diag_test) < 1e-12, msg='Failed to biorthogonalise')
+
+        # Test we have preserved the overlap
+        self.assertTrue(abs(np.linalg.det(Snew) - np.linalg.det(Sold)) < 1e-12, msg='Failed to conserve overlap')
+
+if __name__=='__main__':
+    unittest.main()
