30c3a4fe10bae3afc2a107c2474754f804aa3ff1
Hugh Burton <hb407@cam.ac.uk>
2022-08-05 16:11:19 +0100

    Removed surplus files

--- src/newton_raphson.py
+++ src/newton_raphson.py
@@ -1,11 +1,4 @@
-
-import os
-os.environ["OMP_NUM_THREADS"] = "1" # export OMP_NUM_THREADS=1
-os.environ["OPENBLAS_NUM_THREADS"] = "1" # export OPENBLAS_NUM_THREADS=1
-os.environ["MKL_NUM_THREADS"] = "1" # export MKL_NUM_THREADS=1
-os.environ["VECLIB_MAXIMUM_THREADS"] = "1" # export VECLIB_MAXIMUM_THREADS=1
-os.environ["NUMEXPR_NUM_THREADS"] = "1" # export NUMEXPR_NUM_THREADS=1
-
+#!/usr/bin/python3
 
 import datetime
 import numpy as np
