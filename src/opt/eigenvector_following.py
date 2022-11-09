fixed Orb-CI hessian

src/opt/eigenvector_following.py
@@ -91,7 +91,6 @@ class EigenFollow:
             istep += 1
 
         if plev>0: print("  ================================================================")
-        print(hess_eig)
         kernel_end_time = datetime.datetime.now() # Save end time
         computation_time = kernel_end_time - kernel_start_time
         if plev>0: print("  Eigenvector-following walltime: ", computation_time.total_seconds(), " seconds")
@@ -131,10 +130,9 @@ class EigenFollow:
                 # Downhill step for this direction
                 qn_t[i] = - 2.0 * gt[i] / denom
                 sd_t[i] = - gt[i]
-#            print("{: 10.6f} {: 10.6f} {: 10.6f} {: 10.6f}".format(hess_eig[i],gt[i],sd_t[i],qn_t[i]))
 
-        #qn_t = np.clip(qn_t, -self.control["maxstep"], self.control["maxstep"])
-        #sd_t = np.clip(sd_t, -self.control["maxstep"], self.control["maxstep"])
+#        qn_t = np.clip(qn_t, -self.control["maxstep"], self.control["maxstep"])
+#        sd_t = np.clip(sd_t, -self.control["maxstep"], self.control["maxstep"])
 
         # Get unconstrained minimisation step
         alpha = - np.dot(gt, sd_t) / np.einsum('i,i,i', sd_t, hess_eig, sd_t)

src/search.py
@@ -71,7 +71,6 @@ if __name__ == '__main__':
 
     nmo = myhf.mo_coeff.shape[1]
     ndet = mycas.nDet
-    print(ndet)
 
     # Get inital coefficients and CI vectors
     ref_mo = myhf.mo_coeff.copy()
@@ -87,19 +86,16 @@ if __name__ == '__main__':
 
         # Set orbital coefficients
         mycas.initialise(mo_guess, ci_guess)
-        num_hess = mycas.get_numerical_hessian(eps=1e-5)
-        hess = mycas.hessian
-        print("Numerical Hessian")
-        print(num_hess)
-        print("Hessian")
-        print(hess)
-        print("Hessian")
-        print(np.linalg.eigvalsh(num_hess))
-        print(np.linalg.eigvalsh(hess))
-        print("Gradient")
-        print(mycas.get_numerical_gradient())
-        print(mycas.gradient)
-        quit()
+        #num_hess = mycas.get_numerical_hessian(eps=1e-5)
+        #hess = mycas.hessian
+        #print("Numerical Hessian")
+        #print(num_hess)
+        #print("Hessian")
+        #print(hess)
+        #print("Hessian")
+        #print(np.linalg.eigvalsh(num_hess))
+        #print(np.linalg.eigvalsh(hess))
+        #quit()
 
         opt = EigenFollow(minstep=0.0,rtrust=0.15)
         if not opt.run(mycas, thresh=thresh, maxit=500, index=Hind):

src/ss_casscf.py
@@ -80,11 +80,11 @@ class ss_casscf():
 
     def get_ao_integrals(self):
         self.enuc       = self._scf.energy_nuc()
-        self.v1e        = self.mol.intor('int1e_nuc')       # Nuclear repulsion matrix elements
-        self.t1e        = self.mol.intor('int1e_kin')       # Kinetic energy matrix elements
-        self.hcore      = self.t1e + self.v1e               # 1-electron matrix elements in the AO basis
+        self.v1e        = self.mol.intor('int1e_nuc')  # Nuclear repulsion matrix elements
+        self.t1e        = self.mol.intor('int1e_kin')  # Kinetic energy matrix elements
+        self.hcore      = self.t1e + self.v1e          # 1-electron matrix elements in the AO basis
         self.norb       = self.hcore.shape[0]
-        self.ovlp       = self.mol.intor('int1e_ovlp')      # Overlpa matrix
+        self.ovlp       = self.mol.intor('int1e_ovlp') # Overlpa matrix
        
 
     def initialise(self, mo_guess, ci_guess):
@@ -102,7 +102,8 @@ class ss_casscf():
 
     @property
     def energy(self):
-        ''' Compute the energy corresponding to a given set of one-el integrals, two-el integrals, 1- and 2-RDM '''
+        ''' Compute the energy corresponding to a given set of
+             one-el integrals, two-el integrals, 1- and 2-RDM '''
         E  = self.energy_core
         E += np.einsum('pq,pq', self.h1eff, self.dm1_cas)
         E += 0.5 * np.einsum('pqrs,pqrs', self.h2eff, self.dm2_cas)
@@ -124,30 +125,12 @@ class ss_casscf():
     @property
     def hessian(self):
         ''' This method concatenate the orb-orb, orb-CI and CI-CI part of the Hessian '''
-        #self.eri = ao2mo.incore.full(self._scf._eri, self.mo_coeff, compact=False).reshape((self.nmo,)*4)
-        H_OrbOrb = 0*(self.get_hessianOrbOrb()[:,:,self.rot_idx])[self.rot_idx,:]
-        H_CICI   = 0*self.get_hessianCICI()
+        H_OrbOrb = (self.get_hessianOrbOrb()[:,:,self.rot_idx])[self.rot_idx,:]
+        H_CICI   = self.get_hessianCICI()
         H_OrbCI  = self.get_hessianOrbCI()[self.rot_idx,:]
 
         return np.block([[H_OrbOrb, H_OrbCI],
                          [H_OrbCI.T, H_CICI]])
-
-    def test_ham(self):
-        print("test_ham")
-        self.eri = ao2mo.incore.full(self._scf._eri, self.mo_coeff, compact=False).reshape((self.nmo,)*4)
-        htest = np.zeros((self.nDet,self.nDet))
-        mat_id = np.identity(self.nDet)
-        for i in range(self.nDet):
-            for j in range(self.nDet):
-                dm1_cas, dm2_cas = self.get_tCASRDM12(mat_id[i],mat_id[j])
-                dm1 = self.CASRDM1_to_RDM1(dm1_cas,i!=j)
-                dm2 = self.CASRDM2_to_RDM2(dm1_cas,dm2_cas,i!=j)
-                if(i==j): htest[i,j] += self.enuc
-                htest[i,j] += np.einsum('pq,qp',self.h1e_mo,dm1) 
-                htest[i,j] += 0.5 * np.einsum('pqrs,pqrs',self.eri,dm2)
-        print(htest)
-        print(self.ham)
-        quit()
 
     def get_hessian_index(self, tol=1e-16):
         eigs = scipy.linalg.eigvalsh(self.hessian)
@@ -160,11 +143,11 @@ class ss_casscf():
             else:         nzero +=1 
         return ndown, nzero, nuphl
 
+
     def pushoff(self, n, angle=np.pi/2):
         """Perturb along n Hessian directions"""
         eigval, eigvec = np.linalg.eigh(self.hessian)
         step = sum(eigvec[:,i] * angle for i in range(n))
-        print(eigval[:n])
         self.take_step(step)
     
     def guess_casci(self, n):
@@ -320,10 +303,6 @@ class ss_casscf():
 
     def get_orbital_gradient(self):
         ''' This method builds the orbital part of the gradient '''
-        self.test_ham()
-        quit()
-        self.get_gen_fock(self.dm1_cas,self.dm2_cas)
-
         g_orb = np.zeros((self.norb,self.norb))
         ncore = self.ncore
         ncas  = self.ncas
@@ -392,7 +371,8 @@ class ss_casscf():
                 for j in range(ncore):
                     for k in range(ncore):
                         for l in range(ncore):
-                            dm2[i,j,k,l] = 4 * delta_kron(i,j) * delta_kron(k,l) - 2 * delta_kron(i,l) * delta_kron(k,j)
+                            dm2[i,j,k,l]  = 4 * delta_kron(i,j) * delta_kron(k,l) 
+                            dm2[i,j,k,l] -= 2 * delta_kron(i,l) * delta_kron(k,j)
 
                     for p in range(ncore,nocc):
                         for q in range(ncore,nocc):
@@ -451,147 +431,20 @@ class ss_casscf():
 
         return t_dm1_cas.T
 
-    def get_ham_commutator(self):
-        ''' This method build the Hamiltonian commutator matrices '''
-        ncore = self.ncore; ncas = self.ncas; norb = self.norb
-        nocc = ncore + ncas; nvir = norb - nocc
-
-        # Initialise output
-        H_ai = np.zeros((nvir,ncore,self.nDet,self.nDet))
-        H_at = np.zeros((nvir,ncas, self.nDet,self.nDet))
-        H_ti = np.zeros((ncas,ncore,self.nDet,self.nDet))
-
-        # Compute contribution for each CI contribution
-        mat_id = np.identity(self.nDet)
-        two_el = 0
-        for i in range(self.nDet):
-            for j in range(i,self.nDet):
-                dm1_cas, dm2_cas = self.get_tCASRDM12(mat_id[i],mat_id[j])
-                dm1 = self.CASRDM1_to_RDM1(dm1_cas,True)
-                dm2 = self.CASRDM2_to_RDM2(dm1_cas,dm2_cas,True)
-
-                ncore = self.ncore
-                ncas  = self.ncas
-                nocc  = ncore + ncas
-                nvir  = self.norb - nocc
-                nmo   = self.nmo
-
-                if ncore>0 and nvir>0:
-                    one_el = ( np.einsum('pa,pi->ai', self.h1e[:,nocc:], dm1[:,:ncore]) 
-                             + np.einsum('ap,ip->ai', self.h1e[nocc:,:], dm1[:ncore,:]) )
-                    two_el = ( np.einsum('pars,pirs->ai', self.eri[:,nocc:,:,:], dm2[:,:ncore,:,:]) 
-                             + np.einsum('aqrs,iqrs->ai', self.eri[nocc:,:,:,:], dm2[:ncore,:,:,:]) 
-                             + np.einsum('pqra,pqri->ai', self.eri[:,:,:,nocc:], dm2[:,:,:,:ncore]) 
-                             + np.einsum('pqas,pqis->ai', self.eri[:,:,nocc:,:], dm2[:,:,:ncore,:]) )
-                    H_ai[:,:,i,j] = ( one_el + two_el)
-
-                if nvir>0:
-                    # active-virtual
-                    one_el = ( np.einsum('pa,pt->at', self.h1e[:,nocc:], dm1[:,ncore:nocc]) 
-                             + np.einsum('ap,tp->at', self.h1e[nocc:,:], dm1[ncore:nocc,:]) )
-                    two_el = ( np.einsum('pars,ptrs->at', self.eri[:,nocc:,:,:], dm2[:,ncore:nocc,:,:])  
-                             + np.einsum('aqrs,tqrs->at', self.eri[nocc:,:,:,:], dm2[ncore:nocc,:,:,:]) 
-                             + np.einsum('pqra,pqrt->at', self.eri[:,:,:,nocc:], dm2[:,:,:,ncore:nocc]) 
-                             + np.einsum('pqas,pqts->at', self.eri[:,:,nocc:,:], dm2[:,:,ncore:nocc,:]) )
-                    H_at[:,:,i,j] = (one_el + two_el)
-
-                if ncore>0:
-                    # Core active
-                    one_el = ( np.einsum('pt,pi->ti', self.h1e[:,ncore:nocc], dm1[:,:ncore]) 
-                             - np.einsum('pi,pt->ti', self.h1e[:,:ncore], dm1[:,ncore:nocc]) 
-                             + np.einsum('tp,ip->ti', self.h1e[ncore:nocc,:], dm1[:ncore,:]) 
-                             - np.einsum('ip,tp->ti', self.h1e[:ncore,:], dm1[ncore:nocc,:]) )
-                    two_el = ( np.einsum('ptrs,pirs->ti', self.eri[:,ncore:nocc,:,:], dm2[:,:ncore,:,:]) 
-                             - np.einsum('pirs,ptrs->ti', self.eri[:,:ncore,:,:], dm2[:,ncore:nocc,:,:]) 
-                             + np.einsum('tqrs,iqrs->ti', self.eri[ncore:nocc,:,:,:], dm2[:ncore,:,:,:]) 
-                             - np.einsum('iqrs,tqrs->ti', self.eri[:ncore,:,:,:], dm2[ncore:nocc,:,:,:]) 
-                             + np.einsum('pqrt,pqri->ti', self.eri[:,:,:,ncore:nocc], dm2[:,:,:,:ncore]) 
-                             - np.einsum('pqri,pqrt->ti', self.eri[:,:,:,:ncore], dm2[:,:,:,ncore:nocc]) 
-                             + np.einsum('pqts,pqis->ti', self.eri[:,:,ncore:nocc,:], dm2[:,:,:ncore,:]) 
-                             - np.einsum('pqis,pqts->ti', self.eri[:,:,:ncore,:], dm2[:,:,ncore:nocc,:]) )
-                    H_ti[:,:,i,j] = one_el + two_el
-                H_ti[:,:,j,i] = np.conjugate(H_ti[:,:,i,j])
-
-        return H_ai, H_at, H_ti
 
     def get_hessianOrbCI(self):
-        ''' This method build the orb-CI part of the hessian '''
-        ncore = self.ncore; ncas = self.ncas; nocc = ncore + ncas
-        nvir = self.norb - nocc
-
+        '''This method build the orb-CI part of the hessian'''
         H_OCI = np.zeros((self.norb,self.norb,self.nDet-1))
-        mat_ci = self.mat_ci
+        for k in range(1,self.nDet):
 
-        H_ai, H_at, H_ti = self.get_ham_commutator()
+            # Get transition density matrices
+            dm1_cas, dm2_cas = self.get_tCASRDM12(self.mat_ci[:,0], self.mat_ci[:,k])
 
-        ci0 = mat_ci[:,0]
+            # Get transition generalised Fock matrix
+            F = self.get_gen_fock(dm1_cas, dm2_cas, True)
 
-        h1e = self.h1e
-        self.eri = ao2mo.incore.full(self._scf._eri, self.mo_coeff, compact=False).reshape((self.nmo,)*4)
-        eri = self.eri
-
-        mat_id = np.identity(self.nDet)
-        print("get_hessianOrbCI")
-        for k in range(len(mat_ci)-1): # Loop on Hessian indices
-                print(k)
-                dm1_cas, dm2_cas = self.get_tCASRDM12(self.mat_ci[:,0], self.mat_ci[:,k+1])
-                F = self.get_gen_fock(dm1_cas, dm2_cas, True)
-                #print("GenFock")
-                #print(F)
-                #print("F - F.T")
-                #print(F-F.T)
-                #print("test...")
-                #print(4*(F-F.T)[self.rot_idx])
-                #print(2*(F-F.T)[self.rot_idx])
-                #print((F-F.T)[self.rot_idx])
-
-                #ncore = self.ncore
-                #ncas  = self.ncas
-                #nocc  = ncore + ncas
-                #nvir  = self.norb - nocc
-                #nmo   = self.nmo
-
-                ## Gradient computation from mc1step
-                #jkcaa = np.empty((nocc,ncas))
-                #vhf_a = np.empty((nmo,nmo))
-                #dm2tmp = dm2_cas.transpose(1,2,0,3) + dm2_cas.transpose(0,2,1,3)
-                #dm2tmp = dm2tmp.reshape(ncas**2,-1)
-                #hdm2   = np.empty((nmo,ncas,nmo,ncas))
-                #g_dm2  = np.empty((nmo,ncas))
-                #for i in range(nmo):
-                #    jbuf = self._eri.ppaa[i]
-                #    kbuf = self._eri.papa[i]
-                #    if i < nocc: jkcaa[i] = np.einsum('ik,ik->i', 6 * kbuf[:,i] - 2 * jbuf[i], dm1_cas)
-                #    vhf_a[i] =(np.einsum('quv,uv->q', jbuf, dm1_cas) -
-                #               np.einsum('uqv,uv->q', kbuf, dm1_cas) * 0.5)
-                #    jtmp = lib.dot(jbuf.reshape(nmo,-1), dm2_cas.reshape(self.ncas*self.ncas,-1))
-                #    jtmp = jtmp.reshape(nmo,ncas,ncas)
-                #    ktmp = lib.dot(kbuf.transpose(1,0,2).reshape(self.nmo,-1), dm2tmp)
-                #    hdm2[i] = (ktmp.reshape(self.nmo,self.ncas,self.ncas)+jtmp).transpose(1,0,2)
-                #    g_dm2[i] = np.einsum('uuv->v', jtmp[ncore:nocc])
-                #jbuf = kbuf = jtmp = ktmp = dm2tmp = None
-                #vhf_ca = self._eri.vhf_c + vhf_a
-
-
-                #Htmp = np.zeros_like(self.h1e_mo)
-                #Htmp[:,:ncore] = (self.h1e_mo[:,:ncore] + vhf_ca[:,:ncore]) * 2
-                #Htmp[:,ncore:nocc] = np.dot(self.h1e_mo[:,ncore:nocc]+self._eri.vhf_c[:,ncore:nocc],dm1_cas)
-                #Htmp[:,ncore:nocc] += g_dm2
-
-                #cleft = mat_ci[:,k+1]
-                #if ncore>0 and nvir>0:
-                #    # ERROR
-                #    H_OCI[nocc:, :ncore, k] = 2*np.einsum('k,aikl,l->ai', cleft, H_ai, ci0)
-                #if nvir>0:
-                #    # ERROR
-                #    H_OCI[nocc:, ncore:nocc, k] = 2*np.einsum('k,aikl,l->ai', cleft, H_at, ci0)
-                #if ncore>0:
-                #    # Fine
-                #    H_OCI[ncore:nocc, :ncore, k] = 2*np.einsum('k,aikl,l->ai', cleft, H_ti, ci0)
-
-                H_OCI[:,:,k] = 2*(F - F.T)
-
-        H_OCI = H_OCI #+ np.einsum('pqs->qps',H_OCI)
+            # Save component
+            H_OCI[:,:,k-1] = 2*(F - F.T)
 
         return H_OCI
 
@@ -700,68 +553,11 @@ class ss_casscf():
         ''' This method build the CI-CI part of the hessian '''
         if(self.nDet > 1):
             e0 = np.einsum('i,ij,j', self.mat_ci[:,0], self.ham, self.mat_ci[:,0])
-            return 2.0 * np.einsum('ki,kl,lj->ij', self.mat_ci[:,1:], self.ham - e0 * np.identity(self.nDet), self.mat_ci[:,1:])
+            return 2.0 * np.einsum('ki,kl,lj->ij', 
+                    self.mat_ci[:,1:], self.ham - e0 * np.identity(self.nDet), self.mat_ci[:,1:])
         else: 
             return np.zeros((0,0))
 
-    def get_metric(self):
-        met_CICI   = self.get_metricCICI()
-        met_OrbCI  = self.get_metricOrbCI()[self.rot_idx,:]
-        met_OrbOrb = self.get_metricOrbOrb()[self.rot_idx,:][:,self.rot_idx]
-
-        return np.block([[met_OrbOrb,  met_OrbCI],
-                         [met_OrbCI.T, met_CICI]])
-
-    def get_metricOrbOrb(self):
-
-        norb = self.norb; ncore = self.ncore; ncas = self.ncas
-        nocc = ncore + ncas; nvir = norb - nocc
-
-        met = np.zeros((norb,norb,norb,norb))
-        dm1_cas, dm2_cas = self.get_casrdm_12()
-
-        if ncore>0 and nvir>0:
-            for i in range(ncore):
-                for a in range(nocc,norb):
-                    met[a,i,a,i] = 1
-
-        if nvir>0:
-            for a in range(nocc,norb):
-                for t in range(ncore,nocc):
-                    for u in range(ncore,nocc):
-                        dm_tt = dm1_cas[t-ncore,t-ncore]
-                        dm_uu = dm1_cas[u-ncore,u-ncore]
-                        met[a,t,a,u] = dm1_cas[t-ncore,u-ncore]/np.sqrt(dm_tt * dm_uu)
-
-        if ncore>0:
-            for i in range(ncore):
-                for t in range(ncore,nocc):
-                    for u in range(ncore,nocc):
-                        m_t = 2 - dm1_cas[t-ncore,t-ncore]
-                        m_u = 2 - dm1_cas[u-ncore,u-ncore]
-                        met[i,t,i,u] = (2*delta_kron(t,u) - dm1_cas[u-ncore,t-ncore])/np.sqrt(m_t * m_u)
-
-        return met
-        
-
-    def get_metricOrbCI(self):
-        '''Build the orbital-CI component of the metric'''
-        met = np.zeros((self.norb,self.norb,self.nDet-1))
-
-        mat_ci = self.mat_ci
-        ci0 = mat_ci[:,0]
-
-        for k in range(len(mat_ci)-1):
-            ciK = mat_ci[:,k+1]
-            dm1_cas = self.get_tCASRDM1(ciK, ci0)
-            dm1 = self.CASRDM1_to_RDM1(dm1_cas,True)
-            met[:,:,k] = dm1 - dm1.T
-            print(met[:,:,k])
-
-        return met
-
-    def get_metricCICI(self):
-        return np.identity(self.nDet-1)
 
     def _eig(self, h, *args):
         return scf.hf.eig(h, None)
@@ -774,8 +570,9 @@ class ss_casscf():
         return test
     def canonicalize_(self):
         # Compute canonicalised natural orbitals
-        self.mo_coeff, ci, self.mo_energy = mcscf.casci.canonicalize(self, self.mo_coeff, ci=self.mat_ci[:,0], 
-                                                       eris=self._eri, sort=True, cas_natorb=True, casdm1=self.dm1_cas)
+        self.mo_coeff, ci, self.mo_energy = mcscf.casci.canonicalize(
+                      self, self.mo_coeff, ci=self.mat_ci[:,0], 
+                      eris=self._eri, sort=True, cas_natorb=True, casdm1=self.dm1_cas)
 
         # Insert new "occupied" ci vector
         self.mat_ci[:,0] = ci.ravel()
@@ -854,9 +651,6 @@ class ss_casscf():
 
                 Hess[i,j] = ((E1 - E2) - (E3 - E4)) / (4 * eps * eps)
                 if(i!=j): Hess[j,i] = Hess[i,j]
-
-        Hess[:self.nrot,:self.nrot] = 0
-        Hess[self.nrot:,self.nrot:] = 0
         return Hess
 
 