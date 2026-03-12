75a290652a56cbdbf97731ebf6d26a277bfb0ea1
cyzbrown <camhuangbrown@hotmail.co.uk>
2026-03-12 23:40:19 +0000

    small changes

--- examples/test_diis.py
+++ examples/test_diis.py
@@ -1,16 +1,16 @@
 from quantel.ints.pyscf_integrals import PySCFMolecule, PySCFIntegrals
 from quantel.opt.lbfgs import LBFGS
 from quantel.opt.diis import DIIS
-import numpy as np
 from quantel.utils.linalg import matrix_print
+import numpy as np
 np.set_printoptions(linewidth=1000,precision=6,suppress=True)
 
 # Set Occupation selector 
-mom_method = "MOM"
+mom_methods = ["Aufbau", "MOM", "IMOM"] 
 
 if __name__ == "__main__":
     print("\n===============================================")
-    print(f" Testing (I/MOM)-DIIS opt. on CSF and ROKS objects")
+    print(f" Testing DIIS opt. on CSF and ROKS objects")
     print("===============================================")
     # Setup molecule and integrals
     mol  = PySCFMolecule("mol/formaldehyde.xyz","aug-cc-pvdz", "angstrom")
@@ -26,19 +26,16 @@ if __name__ == "__main__":
         mf = WFN(ints,'cs') 
         mf.get_orbital_guess(method="gwh") 
         LBFGS().run(mf, thresh=1e-7)
-        # Localise orbitals in shellis 
-        mf.localise() 
-       
-        # Initialise and optimise open shell singlet state  
-        wfn = WFN(ints, '+-', mom_method=mom_method, scale_core_dens=True)
-        # Select reference orbitals
-        wfn.initialise(mo_guess=mf.mo_coeff) 
-        #wfn.get_orbital_guess(method='rohf',reorder=True)
-        #Cguess = wfn.mo_coeff.copy()
-        #wfn.update() 
-        DIIS().run(wfn, thresh=1e-7, maxit=500, max_vec=6)
-        wfn.canonicalize()
-        wfn.get_davidson_hessian_index(approx_hess=False)
+        mf.localise_orbitals() 
+        for mom_method in mom_methods: 
+            print("\n===============================================")
+            print(f" Testing Method: {mom_method}")
+            print("===============================================")
+            wfn = WFN(ints, '+-', mom_method=mom_method, scale_core_dens=True)
+            wfn.initialise(mo_guess=mf.mo_coeff) 
+            DIIS(max_vec=6).run(wfn, thresh=1e-7, maxit=500)
+            #wfn.canonicalize()
+            #wfn.get_davidson_hessian_index(approx_hess=False)
         
 
 
