3be472eb590d0c1e8bc4f1c69e0d354f7d44b3bd
cyzbrown <camhuangbrown@hotmail.co.uk>
2026-07-21 22:03:07 +0100

    three and body operators via resolution of identity

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -149,6 +149,7 @@ PYBIND11_MODULE(_quantel, m) {
           .def(py::init<>(), "Default Constructor")
           .def("one_body_coupling", &GUGAEval::one_body_coupling, " Compute one body matrix element" ) 
           .def("two_body_coupling", &GUGAEval::two_body_coupling, " Compute two body matrix element" ) 
+          .def("resolve_diag_threeish_body_matrix_element", &GUGAEval::resolve_diag_threeish_body_matrix_element, " Compute threeish body matrix element" ) 
           .def("resolve_two_body_matrix_element", &GUGAEval::resolve_two_body_matrix_element, " Compute two body matrix element" ); 
           
      py::class_<GUGA_CIspace>(m, "GUGA_CIspace")
